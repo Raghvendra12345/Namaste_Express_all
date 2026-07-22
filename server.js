3eac8abb8c4378e23b21090a4e4f9341e6715b9e
Raghvendra12345 <103117995+Raghvendra12345@users.noreply.github.com>
2026-07-22 11:32:41 +0530

    Password Encryption

--- server.js
+++ server.js
@@ -2,46 +2,42 @@ const express = require("express");
 require("./config/db.js");
 const PORT = 7800;
 const User = require("./models/user.js");
-console.log(User);
-console.log(typeof User);
+const bcrypt = require("bcrypt");
 
 const app = express();
 
 app.use(express.json()); //using express json to convert json object into javascript object
 
+app.patch("/update/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  const data = req.body;
 
-app.patch('/update/:userId',async(req,res)=>{
-  const userId=req.params.userId
-  const data=req.body
+  try {
+    const AllowedUpdates = ["age", "skills", "photoUrl", "about", "gender"];
 
-  try{
-     const AllowedUpdates=["age","skills","photoUrl","about","gender"]
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      AllowedUpdates.includes(k),
+    );
 
-     const isUpdateAllowed=Object.keys(data).every((k)=>
-                AllowedUpdates.includes(k)
-     )
-     
-     if(!isUpdateAllowed){
-      return res.status(400).json({error:"Update Not allowed for this section"})
-     }
+    if (!isUpdateAllowed) {
+      return res
+        .status(400)
+        .json({ error: "Update Not allowed for this section" });
+    }
 
-   
-   if(data.skills && data.skills.length>7){
-    return res.status(400).json({error:"Can't add more skills than 7"})
-   }
+    if (data.skills && data.skills.length > 7) {
+      return res.status(400).json({ error: "Can't add more skills than 7" });
+    }
 
-    const user=await User.findByIdAndUpdate({_id:userId},data,{
-      returnDocument:"after",
-      runValidators:true
-    })
-    res.status(200).json(user)
-
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json("Something went wrong" + err.message);
   }
-  catch (err) {
-    res.status(500).json( "Something went wrong" +err.message);
-  }
-  
-})
+});
 
 //GET All Data
 app.get("/getdata", async (req, res) => {
@@ -68,32 +64,27 @@ app.get("/onedata", async (req, res) => {
 });
 
 //DELETE data
-app.delete("/deleteId", async (req, res) => {
-  const userId=req.body._id
+app.delete("/deleteId/:id", async (req, res) => {
+  const userId=req.params.id
   try {
-    console.log("Body:", req.body);
-    console.log("User ID:", userId);
-    const user = await User.findByIdAndDelete({_id:userId});
-
     
+    const user = await User.findByIdAndDelete(userId);
 
     if (!userId) {
       return res.status(400).json({
         message: "userId is required",
       });
-    } 
-
-  
+    }
 
     if (!user) {
-            return res.status(404).json({
-                message: "User not found"
-            });
-        }
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
 
     res.status(200).json({ message: "User Deleted successfully" });
   } catch (err) {
-    res.status(500).json({ error: "Something went wrong" });
+    res.status(500).json({ error: "Something went wrong" +err.message});
     console.log(err.message);
   }
 });
@@ -101,26 +92,62 @@ app.delete("/deleteId", async (req, res) => {
 app.post("/signup", async (req, res) => {
   console.log("Request received");
 
+  const { firstName, lastName, email, password } = req.body;
+
   //Creating insantance of the User Model
-  const user = new User(req.body);
+
   try {
-    //     const user=new User({
-    //      firstName:"Stuart",
-    //     lastName:"Houser",
-    //     email:"stuart@gmail.com",
-    //    password:"12345"
-    // }
-    //    );
-    //    console.log("Before save");
-    //    await user.save();
-    //    console.log("After save");
+    const saltrounds = 20;
+
+    const passwordHash = await bcrypt.hash(password, saltrounds);
+    console.log(passwordHash);
+
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
+
+    const existingUser = await User.findOne({
+      email,
+    });
+    if (existingUser) {
+      res.status(401).json({ error: "It is a used id" });
+    }
+
     console.log(req.body);
     await user.save();
 
     res.status(201).json({ message: "User saved successfully" });
   } catch (err) {
-    res.status(500).json({ error:err.message });
+    res.status(500).json({ error: err.message });
     console.log(err.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({email})
+
+    if(!user){
+      return es.status(402).json({error:"User not found"})
+    }
+
+    const isPasswordValid = await bcrypt.compare(password,user.password);
+
+    if(!isPasswordValid){
+      return res.status(401).json({error:'Password is not valid'})
+    }
+
+    
+    res.status(200).json({message:'User LoggedIn Successfully'})
+    console.log('User Logged In')
+  } catch (err) {
+    res.status(500).json(err.message)
+    console.log(err.message)
   }
 });
 
