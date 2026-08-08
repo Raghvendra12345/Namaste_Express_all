297a47fe49448795fe7b68d16b30dfea9e1ea7cf
Raghvendra12345 <103117995+Raghvendra12345@users.noreply.github.com>
2026-08-09 00:41:25 +0530

    feed Api

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const express=require("express")
 const userAuth = require("../Midddleware&Error_Handling/auth")
 const userRouter=express.Router()
 const ConnectionRequest=require("../models/connectionRequest")
+const User=require("../models/user")
 
 const User_Safe_Data="firstName lastName photUrl age gender about skills"
 
@@ -48,5 +49,55 @@ userRouter.get('/user/connections',userAuth,async(req,res)=>{
      catch(err){
          res.status(500).json({message:err.message})
      }
+})
+
+userRouter.get('/feed',userAuth,async(req,res)=>{
+    try{
+        
+
+        //User should see all the users cards except
+        //0. his own card
+        //1.his connections
+        //2.ignored people
+        //3.already sent the connection request
+
+
+
+
+       const loggedInUser=req.user
+
+
+       //Find all connection requests (sent + received)
+       const connectionRequest=await ConnectionRequest.find({
+        $or:[
+            {toUserId:loggedInUser._id},
+            {fromUserId:loggedInUser._id}
+        ]
+       }).select("fromUserId toUserId")
+    //    .populate("fromUserId","firstName")
+    //    .populate("toUserId","firstName")
+
+      
+    const hideUsersFromFeed=new Set();
+    connectionRequest.forEach((req)=>{
+        hideUsersFromFeed.add(req.fromUserId.toString())
+        hideUsersFromFeed.add(req.toUserId.toString())
+    })
+
+    
+
+    const users=await User.find({
+        $and:[
+            {_id: {$nin:Array.from(hideUsersFromFeed)}},    //making set database again back to array
+            {_id:{$ne:loggedInUser._id}}
+        ]
+    }).select(User_Safe_Data)
+
+
+       res.send(users)
+    }
+    catch(err){
+      res.status(500).json({message:err.message})
+    }
 })
 module.exports=userRouter
